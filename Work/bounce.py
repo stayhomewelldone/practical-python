9894aad5d9e13063d62637c7a00595b224599ac4
stayhomewelldone <0973013@hr.nl>
2024-12-30 21:02:19 +0100

    Made the exercise named "bounce.py"

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+
+bounces = 1
+
+
+while bounces < 11:
+    
+    height = round( height /5 *3, 4) 
+    print( bounces, height)
+    bounces = bounces + 1
+
