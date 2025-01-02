293e4ff3853c14a3f00b8bf770ba2809fb328217
stayhomewelldone <0973013@hr.nl>
2025-01-02 01:51:34 +0100

    Refactored code to a better variant.

--- Work/pcost.py
+++ Work/pcost.py
@@ -7,7 +7,7 @@ with open('Data/portfolio.csv', 'rt') as f:
     headers = next(f)
     for line in f: 
         row = line.split(',')
-        total_cost = total_cost + (int(row[1]) * float(row[2]))
+        total_cost += (int(row[1]) * float(row[2]))
 
 
 print(f'Total cost {round(total_cost,2 )}')
