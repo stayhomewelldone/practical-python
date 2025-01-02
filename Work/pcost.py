0ea02588975fef149db8417701025706a30ff827
stayhomewelldone <0973013@hr.nl>
2025-01-02 01:41:05 +0100

    Exercise 1.17 done.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+total_cost = 0 
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f: 
+        row = line.split(',')
+        total_cost = total_cost + (int(row[1]) * float(row[2]))
+
+
+print(f'Total cost {round(total_cost,2 )}')
