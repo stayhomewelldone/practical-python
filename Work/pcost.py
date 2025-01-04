606cc78e0fc027244979c1d17be6df25cbe45459
stayhomewelldone <0973013@hr.nl>
2025-01-04 18:31:39 +0100

    Exercise 1.30 done.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,16 +1,21 @@
 # pcost.py
 #
 # Exercise 1.27
-total_cost = 0 
 
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f: 
-        row = line.split(',')
-        total_cost += (int(row[1]) * float(row[2]))
+def portfolio_cost(filename):
+    
+    with open(filename, 'rt') as f:
+        headers = next(f)
+        total_cost = 0
+        for line in f: 
+            row = line.split(',')
+            total_cost += (int(row[1]) * float(row[2]))
+        return round(total_cost,2)
 
+cost = portfolio_cost('Data/portfolio.csv')
 
-print(f'Total cost {round(total_cost,2 )}')
+print(f'Total cost {cost}')
 
 #Exercise 1.29 done
+#Exercise 1.30 done
 
