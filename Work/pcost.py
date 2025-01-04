5d86c0495eae17ab3f1307599bf8600a9f5a19a0
stayhomewelldone <0973013@hr.nl>
2025-01-04 18:41:44 +0100

    Exercise 1.31 done.

--- Work/pcost.py
+++ Work/pcost.py
@@ -8,8 +8,12 @@ def portfolio_cost(filename):
         headers = next(f)
         total_cost = 0
         for line in f: 
-            row = line.split(',')
-            total_cost += (int(row[1]) * float(row[2]))
+            
+                row = line.split(',')
+                try: 
+                    total_cost += (int(row[1]) * float(row[2]))
+                except ValueError:
+                     print("Couldn't parse", line)
         return round(total_cost,2)
 
 cost = portfolio_cost('Data/portfolio.csv')
@@ -18,4 +22,5 @@ print(f'Total cost {cost}')
 
 #Exercise 1.29 done
 #Exercise 1.30 done
+#Exercise 1.31 done
 
