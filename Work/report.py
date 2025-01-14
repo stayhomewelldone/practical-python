ed535a8b2e29616e5ff46429ac890e0b62dd9203
stayhomewelldone <0973013@hr.nl>
2025-01-14 23:12:13 +0100

    Exercise 3.2 and everything before done.

--- Work/report.py
+++ Work/report.py
@@ -5,8 +5,7 @@ import csv
 import sys
 from pprint import pprint
 
-headers = ('Name', 'Shares', 'Price', 'Change')
-def read_portfolio(filename):
+def read_portfolio(filename)->list:
     'Read the contents of a portfolio'
     with open(filename, 'rt') as f:
      
@@ -20,9 +19,8 @@ def read_portfolio(filename):
                      portfolio.append(holding) 
                 except ValueError:
                      print(f'Couldn\'t parse, {row} on line {rowno}')
-        print(portfolio)  
         return portfolio
-def read_prices(filename):
+def read_prices(filename)->dict:
     'Read the contents of prices, and returns a dict'
     with open(filename, 'rt') as f:
      
@@ -38,7 +36,7 @@ def read_prices(filename):
                
         return prices
     
-def calculate_current_portfolio(prices, portfolio):
+def calculate_current_portfolio(prices, portfolio)->list:
      'Calculates and returns the current portfolio value'
      current_portfolio = []
      for row in portfolio:
@@ -65,7 +63,7 @@ def new_value_portfolio(prices, portfolio):
           new_value += entry['share'] * prices[entry['name']]
 
      return new_value
-def make_report(portfolio, prices):
+def make_report(portfolio, prices)->list:
      'To create a visually appealing table, combining data from prices.csv and portfolio.csv'
      report = [] #list of tuples     
      for entry in portfolio:
@@ -74,42 +72,56 @@ def make_report(portfolio, prices):
 
      return report
 
+def print_report(report):
+     '''Function for printing the report'''
+     headers = ('Name', 'Shares', 'Price', 'Change')
+     print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+     print(('-' * 10 + ' ') * len(headers))
+     for name, shares, price, change in report:
+          print(f'{name:>10s} {shares:>10d} {"$" + price:>10} {change:>10.2f}')
+
+def handle_args()->str:
+     '''Handle the args passed by executing of the script.'''
+     if len(sys.argv) == 2:
+          filename = sys.argv[1]
+          filename_prices  = sys.argv[2]
+
+     else:
+          filename = ('Data/portfolio.csv')
+          filename_prices = ('Data/prices.csv')
+     
+     return filename, filename_prices
+def print_new_value_portfolio(current_value,new_value,difference):
+     pprint(f'Value of current portfolio: {current_value}')
+     pprint(f'New value of portfolio:{new_value} ')
+     pprint(difference)
+
+def portfolio_report(portfolio_filename, prices_filename):
+     
+     # filename, filename_prices = handle_args()
+     portfolio = read_portfolio(portfolio_filename)
+     prices = read_prices(prices_filename)
 
 
-if len(sys.argv) == 2:
-     filename_portfolio = sys.argv[1]
-     filename_prices  = sys.argv[2]
+     # current_portfolio = calculate_current_portfolio( prices, portfolio)
+     current_value = current_value_portfolio(portfolio=portfolio)
+     new_value =  new_value_portfolio(prices, portfolio)
+     difference  = round(current_value - new_value, 2)
+     if new_value > current_value :
+          difference = f'Gain of {difference}'
+     else:
+          difference = f'Loss of {difference}'
+     report = make_report(portfolio, prices)
+     print_new_value_portfolio(current_value,new_value,difference)
+     print_report(report)
 
-else:
-     filename = ('Data/portfolio.csv')
-     filename_prices = ('Data/prices.csv')
-portfolio = read_portfolio(filename)
-prices = read_prices(filename_prices)
-
-
-# current_portfolio = calculate_current_portfolio( prices, portfolio)
-current_value = current_value_portfolio(portfolio=portfolio)
-new_value =  new_value_portfolio(prices, portfolio)
-difference  = round(current_value - new_value, 2)
-if new_value > current_value :
-     difference = f'Gain of {difference}'
-else:
-     difference = f'Loss of {difference}'
-report = make_report(portfolio, prices)
-
-print()
+portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
 # pprint(f' Value of current portfolio: {current_portfolio}')
-pprint(f'Value of current portfolio: {current_value}')
-pprint(f'New value of portfolio:{new_value} ')
-pprint(difference)
+
 # for r in report:
 #      print('%10s %10d %10.2f %10.2f' % r)
 
-print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
-print(f'---------- ---------- ---------- -----------')
-for name, shares, price, change in report:
-     print(f'{name:>10s} {shares:>10d} {"$" + price:>10} {change:>10.2f}')
-    
+
 # pprint(f'Portfolio list of dict {portfolio}')
 # pprint(f'Prices dict {prices}')
 
