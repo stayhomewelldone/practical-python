f78942b706b5fc4223b8c1b132eaad6d143f0278
stayhomewelldone <0973013@hr.nl>
2025-01-22 01:43:27 +0100

    Exercise 3.12 done.

--- Work/report.py
+++ Work/report.py
@@ -4,37 +4,42 @@
 import csv
 import sys
 from pprint import pprint
+from fileparse import parse_csv
 
 def read_portfolio(filename)->list:
-    'Read the contents of a portfolio'
-    with open(filename, 'rt') as f:
+     '''Read the contents of a portfolio'''
+     portfolio = parse_csv(filename)
+     return portfolio
+
+#     with open(filename, 'rt') as f:
      
-        rows = csv.reader(f)
-        headers = next(rows)
-        portfolio = [] # Initial, list of dicts
-        for rowno, row in enumerate(rows, start=1): 
-                record = dict(zip(headers,row ))
-                try:
-                     holding = {'name': record['name'],"share": int(record['shares']), 'price': float(record['price']) } 
-                     portfolio.append(holding) 
-                except ValueError:
-                     print(f'Couldn\'t parse, {row} on line {rowno}')
-        return portfolio
+#         rows = csv.reader(f)
+#         headers = next(rows)
+#         portfolio = [] # Initial, list of dicts
+#         for rowno, row in enumerate(rows, start=1): 
+#                 record = dict(zip(headers,row ))
+#                 try:
+#                      holding = {'name': record['name'],"share": int(record['shares']), 'price': float(record['price']) } 
+#                      portfolio.append(holding) 
+#                 except ValueError:
+#                      print(f'Couldn\'t parse, {row} on line {rowno}')
 def read_prices(filename)->dict:
     'Read the contents of prices, and returns a dict'
-    with open(filename, 'rt') as f:
+    prices = parse_csv(filename, types=[str, float], has_headers=False)
+    return prices
+
+#     with open(filename, 'rt') as f:
      
-        rows = csv.reader(f)
-        prices = {} # Initial dict
-        for rowno, row in enumerate(rows): 
-                try:
-                     prices[row[0]] = float(row[1]) 
-                except ValueError:
-                     print(f'Couldn\'t parse, {row} on line {rowno}')
-                except IndexError as e:
-                     print(f'Indexerror occured {row} on line: {rowno}')
+#         rows = csv.reader(f)
+#         prices = {} # Initial dict
+#         for rowno, row in enumerate(rows): 
+#                 try:
+#                      prices[row[0]] = float(row[1]) 
+#                 except ValueError:
+#                      print(f'Couldn\'t parse, {row} on line {rowno}')
+#                 except IndexError as e:
+#                      print(f'Indexerror occured {row} on line: {rowno}')
                
-        return prices
     
 def calculate_current_portfolio(prices, portfolio)->list:
      'Calculates and returns the current portfolio value'
@@ -52,22 +57,22 @@ def current_value_portfolio( portfolio):
      'Calculate the current value of the portfolio'
      current_amount = 0
      for entry in portfolio:
-          current_amount += entry['share'] * entry['price']
+          current_amount += entry['shares'] * entry['price']
 
      return current_amount
 
 def new_value_portfolio(prices, portfolio):
      'Calculate new value of portfolio based on the new prices in prices.csv'
      new_value = 0
-     for entry in portfolio:
-          new_value += entry['share'] * prices[entry['name']]
+     for index, entry in enumerate(portfolio):
+          new_value += entry['shares'] * prices[index][1]
 
      return new_value
 def make_report(portfolio, prices)->list:
      'To create a visually appealing table, combining data from prices.csv and portfolio.csv'
      report = [] #list of tuples     
-     for entry in portfolio:
-         tuple_line = (entry['name'], entry['share'],  str(round(prices[entry['name']], 2)), entry['price'] - prices[entry['name']])
+     for index, entry in enumerate(portfolio):
+         tuple_line = (entry['name'], entry['shares'],  str(round(prices[index][1], 2)), entry['price'] - prices[index][1])
          report.append(tuple_line)
 
      return report
