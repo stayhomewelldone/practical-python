257994f03ceee7f378de1c08dedb3198fe8a0dbf
stayhomewelldone <0973013@hr.nl>
2025-01-28 23:30:06 +0100

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,10 +3,10 @@
 # Exercise 1.27
 import csv
 import sys
-from report import read_portfolio
+import report
 def portfolio_cost(filename):
     'Computes the total cost (shares*price) of a portfolio file'
-    portfolio = read_portfolio(filename)
+    portfolio = report.read_portfolio(filename)
     total_cost = 0.0
     for rowno, row in enumerate(portfolio, start=1): 
         print(row)
