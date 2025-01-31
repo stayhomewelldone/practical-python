eeedf0aea8cb5a5fb722aed686d93d19450e5318
stayhomewelldone <0973013@hr.nl>
2025-01-31 01:25:40 +0100

    Exercise 3.17 done

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -2,49 +2,53 @@
 #
 # Exercise 3.3
 import csv
+from io import TextIOWrapper
 
-def parse_csv(filename, select=None, types=[str, int, float], has_headers=True, delimiter=',', silence_errors=False):
+def parse_csv(lines, select=None, types=[str, int, float], has_headers=True, delimiter=',', silence_errors=False):
     '''
     Parse a CSV file into a list of records
     '''
-    with open(filename) as f:
-        rows = csv.reader(f, delimiter=delimiter)
+    x = isinstance(lines, TextIOWrapper)
+    if not x:
+        lines = open(lines)
+    rows = csv.reader(lines, delimiter=delimiter)
 
-        if has_headers:
-        # Read the file headers
-            headers = next(rows)
 
-        # If a column selector was given, find indices of the specified columns.
-        # Also narrow the set of headers used for resulting dictionaries
-        if select:
-            if  not has_headers:
-                raise RuntimeError('select argument requires column headers')
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
+    if has_headers:
+    # Read the file headers
+        headers = next(rows)
 
-        records = []
+    # If a column selector was given, find indices of the specified columns.
+    # Also narrow the set of headers used for resulting dictionaries
+    if select:
+        if not has_headers:
+            raise RuntimeError('select argument requires column headers')
+        indices = [headers.index(colname) for colname in select]
+        headers = select
+    else:
+        indices = []
+
+    records = []
         
-        for index, row in enumerate(rows):
-            try: 
-                if not row:     # Skip rows with no data
-                    continue
-                # Filter the row if specific columns were selected
-                if indices:
-                    row = [ row[index] for index in indices ]
-                if types: 
-                    row = [ func(val) for func, val in zip(types, row)]
-                # Make a dictionary
-                if has_headers:
-                    record = dict(zip(headers, row))
-                else:
-                    record = tuple(row)
-                records.append(record)
-            except ValueError as e:
-                if not silence_errors:
-                    print(f'Row {index} can\'t be converted {row}')
-                    print(e)
-        
+    for index, row in enumerate(rows):
+        try: 
+            if not row:     # Skip rows with no data
+                continue
+            # Filter the row if specific columns were selected
+            if indices:
+                row = [ row[index] for index in indices ]
+            if types: 
+                row = [ func(val) for func, val in zip(types, row)]
+            # Make a dictionary
+            if has_headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+            records.append(record)
+        except ValueError as e:
+            if not silence_errors:
+                print(f'Row {index} can\'t be converted {row}')
+                print(e)
+    
     return records
 
--- Work/report.py
+++ Work/report.py
@@ -1,7 +1,6 @@
 # report.py
 #
 # Exercise 2.4
-import csv
 import sys
 from pprint import pprint
 from fileparse import parse_csv
@@ -11,65 +10,36 @@ def read_portfolio(filename)->list:
      portfolio = parse_csv(filename)
      return portfolio
 
-#     with open(filename, 'rt') as f:
-     
-#         rows = csv.reader(f)
-#         headers = next(rows)
-#         portfolio = [] # Initial, list of dicts
-#         for rowno, row in enumerate(rows, start=1): 
-#                 record = dict(zip(headers,row ))
-#                 try:
-#                      holding = {'name': record['name'],"share": int(record['shares']), 'price': float(record['price']) } 
-#                      portfolio.append(holding) 
-#                 except ValueError:
-#                      print(f'Couldn\'t parse, {row} on line {rowno}')
 def read_prices(filename)->dict:
-    'Read the contents of prices, and returns a dict'
+    '''Read the contents of prices, and returns a dict'''
     prices = parse_csv(filename, types=[str, float], has_headers=False)
     return prices
-
-#     with open(filename, 'rt') as f:
-     
-#         rows = csv.reader(f)
-#         prices = {} # Initial dict
-#         for rowno, row in enumerate(rows): 
-#                 try:
-#                      prices[row[0]] = float(row[1]) 
-#                 except ValueError:
-#                      print(f'Couldn\'t parse, {row} on line {rowno}')
-#                 except IndexError as e:
-#                      print(f'Indexerror occured {row} on line: {rowno}')
-               
     
 def calculate_current_portfolio(prices, portfolio)->list:
-     'Calculates and returns the current portfolio value'
+     '''Calculates and returns the current portfolio value'''
      current_portfolio = []
      for row in portfolio:
-          
                row['gain/loss'] = round(prices[row['name']] - row['price'] ,2)
                row['price'] = prices[row['name']]
                current_portfolio.append(row)
-
-
      return current_portfolio        
                
 def current_value_portfolio( portfolio):
-     'Calculate the current value of the portfolio'
+     '''Calculate the current value of the portfolio''' 
      current_amount = 0
      for entry in portfolio:
           current_amount += entry['shares'] * entry['price']
-
      return current_amount
 
 def new_value_portfolio(prices, portfolio):
-     'Calculate new value of portfolio based on the new prices in prices.csv'
+     '''Calculate new value of portfolio based on the new prices in prices.csv'''
      new_value = 0
      for index, entry in enumerate(portfolio):
           new_value += entry['shares'] * prices[index][1]
 
      return new_value
 def make_report(portfolio, prices)->list:
-     'To create a visually appealing table, combining data from prices.csv and portfolio.csv'
+     '''To create a visually appealing table, combining data from prices.csv and portfolio.csv'''
      report = [] #list of tuples     
      for index, entry in enumerate(portfolio):
          tuple_line = (entry['name'], entry['shares'],  str(round(prices[index][1], 2)), entry['price'] - prices[index][1])
@@ -103,12 +73,8 @@ def print_new_value_portfolio(current_value,new_value,difference):
 
 def portfolio_report(portfolio_filename, prices_filename):
      
-     # filename, filename_prices = handle_args()
      portfolio = read_portfolio(portfolio_filename)
      prices = read_prices(prices_filename)
-
-
-     # current_portfolio = calculate_current_portfolio( prices, portfolio)
      current_value = current_value_portfolio(portfolio=portfolio)
      new_value =  new_value_portfolio(prices, portfolio)
      difference  = round(current_value - new_value, 2)
@@ -128,14 +94,3 @@ def main(argv):
 if __name__ == '__main__':    
     import sys
     main(sys.argv)
-# pprint(f' Value of current portfolio: {current_portfolio}')
-
-# for r in report:
-#      print('%10s %10d %10.2f %10.2f' % r)
-
-
-# pprint(f'Portfolio list of dict {portfolio}')
-# pprint(f'Prices dict {prices}')
-
-
-# Exercise 2.16 done.
